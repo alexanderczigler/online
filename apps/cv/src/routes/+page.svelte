f33914173b63a71f021e1f285427a9204a5a532e
Alexander Czigler <3116043+alexanderczigler@users.noreply.github.com>
2024-06-08 20:11:12 +0200

    feat: footer

--- apps/cv/src/routes/+page.svelte
+++ apps/cv/src/routes/+page.svelte
@@ -4,3 +4,34 @@
 </script>
 
 <svelte:component this={data.content} />
+
+<section>
+	<p>Alexander Czigler | Curriculum Vitae</p>
+	<p><a href="https://alexanderczigler.se/"> alexanderczigler.se </a></p>
+</section>
+
+<style>
+	section {
+		margin-top: 5rem;
+		border-top: 1px solid #ccc;
+		padding: 1rem 0.5rem;
+		font-size: 0.8rem;
+
+		display: flex;
+		justify-content: space-between;
+	}
+
+	section p {
+		margin: 0;
+	}
+
+	section a,
+	section a:visited {
+		color: var(--link-color);
+		text-decoration: none;
+	}
+
+	section a:hover {
+		font-weight: 400;
+	}
+</style>
