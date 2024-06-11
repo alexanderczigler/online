39a00989f91fceea4e23ba6f26633b58b628756c
Alexander Czigler <3116043+alexanderczigler@users.noreply.github.com>
2024-06-11 10:27:26 +0200

    chore: linting

--- apps/cv/src/routes/+page.ts
+++ apps/cv/src/routes/+page.ts
@@ -5,10 +5,10 @@ export async function load() {
 		const post = await import('../../../../content/cv.md');
 
 		return {
-			content: post.default,
+			content: post.default
 		};
 	} catch (e) {
-    console.error(e);
+		console.error(e);
 		throw error(404, `Could not load CV.`);
 	}
 }
